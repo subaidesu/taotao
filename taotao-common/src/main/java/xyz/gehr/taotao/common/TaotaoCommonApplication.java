bb500f1a1880c888d572dbd61e62645c9bf16ebe
subaidesu <subaidesu@163.com>
2018-04-16 16:11:51 +0800

    create taotao-common

---
+++ taotao-common/src/main/java/xyz/gehr/taotao/common/TaotaoCommonApplication.java
@@ -0,0 +1,12 @@
+package xyz.gehr.taotao.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoCommonApplication.class, args);
+    }
+}
---
+++ taotao-common/src/test/java/xyz/gehr/taotao/common/TaotaoCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.gehr.taotao.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaotaoCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
